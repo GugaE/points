61d5b40a17ea79a4a60bb152e8a7ea5687944b52
Gustavo Enndi <gustavo.enndi.m@gmail.com>
2018-12-20 16:28:19 -0300

    Criação do projeto

---
+++ src/main/java/com/g/point/points/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.g.point.points;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PointsApplication.class);
+	}
+
+}
+
